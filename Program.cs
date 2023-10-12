b0b54195175718f23fac7f9a48dc578bdaf21f3f
edre05001 <leun06001@utb.vaxjo.se>
2023-10-12 11:06:09 +0200

    ez

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// Skriv ut "Snake" baklänges.
+const string input = "SNAKE";
+Stack<char> stack = new Stack<char>();
+
+foreach(char c in input)
+{
+    stack.Push(c);
+}
+
+while(stack.Count != 0)
+{
+    Console.Write(stack.Pop());
+}
